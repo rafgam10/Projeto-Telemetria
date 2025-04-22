12cd41982886e57748e46a694d274fb0dfbca3cf
Rafael <rafgam19@gmail.com>
2025-04-22 09:34:37 -0300

    subido tudo.

--- mobile_app/main.py
+++ mobile_app/main.py
@@ -3,9 +3,15 @@ from kivy.core.window import Window
 from kaki.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivymd.uix.button import MDIconButton
+from kivymd.uix.button import MDIconButton, MDRectangleFlatIconButton
 from kivymd.uix.card import MDCard
+from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
+from kivymd.uix.toolbar import MDTopAppBar  # tudo junto: MDToolbar
 import os
+
+
+#Tamanho da Janela:
+Window.size = (400, 640)  # largura x altura em pixels
 
 class LoginScreen(Screen):
     pass
@@ -13,19 +19,33 @@ class LoginScreen(Screen):
 class HomeScreen(Screen):
     pass
 
+class SuporteScreen(Screen):
+    pass
+
+class PerfilScreen(Screen):
+    pass
+
+class HistoryScreen(Screen):
+    pass
+    
+
 class LoginApp(MDApp, App):
-    Window.size = (360, 640)  # largura x altura em pixels
 
     def build(self):
         Builder.load_file(os.path.join("mobile_app", "screens", "login.kv"))
         Builder.load_file(os.path.join("mobile_app", "screens", "home.kv"))
+        Builder.load_file(os.path.join("mobile_app", "screens", "perfil.kv"))
 
         self.sm = ScreenManager()
         self.sm.add_widget(LoginScreen(name="login"))
         self.sm.add_widget(HomeScreen(name="home"))
+        self.sm.add_widget(SuporteScreen(name="suporte"))
+        self.sm.add_widget(PerfilScreen(name="perfil"))
+        self.sm.add_widget(HistoryScreen(name="historico"))
 
-        return self.sm
+        # return self.sm
         # return Builder.load_file(os.path.join("mobile_app", "screens", "home.kv"))
+        return Builder.load_file(os.path.join("mobile_app", "screens", "suporte.kv"))
 
     def pegar_placa(self):
         placa = self.sm.get_screen("login").ids.placa_input.text
@@ -41,5 +61,8 @@ class LoginApp(MDApp, App):
         self.sm.add_widget(LoginScreen(name="login"))
         self.sm.current = "login"
         return self.sm
+
+    def mudar_tela(self, nome_tela):
+        self.app.current = nome_tela
 
 LoginApp().run()
