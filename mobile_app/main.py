7886fbc638e807878a651a62847685f75c96d29e
rafgam10 <rafgam19@gmail.com>
2025-04-23 13:30:28 -0300

    Código Main.py no momento.

--- mobile_app/main.py
+++ mobile_app/main.py
@@ -2,50 +2,58 @@ from kivymd.app import MDApp
 from kivy.core.window import Window
 from kaki.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDIconButton, MDRectangleFlatIconButton
 from kivymd.uix.card import MDCard
 from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
 from kivymd.uix.toolbar import MDTopAppBar  # tudo junto: MDToolbar
+import matplotlib.pyplot as plt
 import os
 
 
 #Tamanho da Janela:
 Window.size = (400, 640)  # largura x altura em pixels
 
-class LoginScreen(Screen):
+class LoginScreen(MDScreen):
     pass
 
-class HomeScreen(Screen):
+class HomeScreen(MDScreen):
     pass
 
-class SuporteScreen(Screen):
+class SuporteScreen(MDScreen):
     pass
 
-class PerfilScreen(Screen):
+class PerfilScreen(MDScreen):
     pass
 
-class HistoryScreen(Screen):
+class HistoryScreen(MDScreen):
     pass
     
+class ConfigScreen(MDScreen):
+    pass
 
 class LoginApp(MDApp, App):
 
-    def build(self):
+    def build(self, **kwargs):
         Builder.load_file(os.path.join("mobile_app", "screens", "login.kv"))
         Builder.load_file(os.path.join("mobile_app", "screens", "home.kv"))
         Builder.load_file(os.path.join("mobile_app", "screens", "perfil.kv"))
+        Builder.load_file(os.path.join('mobile_app', 'screens', 'historico.kv'))
+        Builder.load_file(os.path.join('mobile_app', 'screens', 'config.kv'))
+        Builder.load_file(os.path.join('mobile_app', 'screens', 'suporte.kv'))
 
-        self.sm = ScreenManager()
+        self.sm = MDScreenManager()
         self.sm.add_widget(LoginScreen(name="login"))
         self.sm.add_widget(HomeScreen(name="home"))
         self.sm.add_widget(SuporteScreen(name="suporte"))
         self.sm.add_widget(PerfilScreen(name="perfil"))
         self.sm.add_widget(HistoryScreen(name="historico"))
+        self.sm.add_widget(ConfigScreen(name="config"))
 
-        # return self.sm
+        return self.sm
         # return Builder.load_file(os.path.join("mobile_app", "screens", "home.kv"))
-        return Builder.load_file(os.path.join("mobile_app", "screens", "suporte.kv"))
+        #return Builder.load_file(os.path.join("mobile_app", "screens", "config.kv"))
 
     def pegar_placa(self):
         placa = self.sm.get_screen("login").ids.placa_input.text
@@ -56,7 +64,7 @@ class LoginApp(MDApp, App):
             self.sm.current = "home"
 
     def ir_para_login(self):
-        self.sm = ScreenManager()
+        self.sm = MDScreenManager()
         Builder.load_file(os.path.join("screens", "login.kv"))
         self.sm.add_widget(LoginScreen(name="login"))
         self.sm.current = "login"
@@ -65,4 +73,5 @@ class LoginApp(MDApp, App):
     def mudar_tela(self, nome_tela):
         self.app.current = nome_tela
 
-LoginApp().run()
+if __name__ == "__main__":
+    LoginApp().run()
