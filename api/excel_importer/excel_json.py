73e5097d982e2a42b9e492e663b01d90aab76406
Rafael <rafgam19@gmail.com>
2025-05-01 17:29:37 -0300

    Código de extração de dados do excel em json, versão 3, com o código de padrão de Datas correto e remoção de colunas vazias e falta de dados.

--- api/excel_importer/excel_json.py
+++ api/excel_importer/excel_json.py
@@ -9,17 +9,25 @@ df = pd.read_excel(arquivo, sheet_name=0)
 # Remove linhas onde 'Data' está vazio
 df = df[df['Data'].notna()]
 
-# Converte a coluna 'Data' para datetime (ex: 23/01/25)
+# Converte colunas de data
 df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%y', errors='coerce')
+df['Data de Saida'] = pd.to_datetime(df['Data de Saida'], format='%d/%m/%y %H:%M:', errors='coerce')
+df['Data de Cheg.'] = pd.to_datetime(df['Data de Cheg.'], format='%d/%m/%y %H:%M:', errors='coerce')
 
 # Filtra os dados a partir de 2025
 df_filtrado = df[df['Data'].dt.year >= 2025]
 
-#Colunas "Unnamed" serão excluídas se contiverem valores 
+# Remove colunas "Unnamed"
 df_filtrado = df_filtrado.loc[:, ~df_filtrado.columns.str.contains('^Unnamed')]
 
-# Exibe os dados ou salva
+# Formata datas para string no padrão dd/mm/yyyy
+df_filtrado['Data'] = df_filtrado['Data'].dt.strftime('%d/%m/%Y')
+df_filtrado['Data de Saida'] = df_filtrado['Data de Saida'].dt.strftime('%d/%m/%Y %H:%M')
+df_filtrado['Data de Cheg.'] = df_filtrado['Data de Cheg.'].dt.strftime('%d/%m/%Y %H:%M')
+
+# Exibe os dados
 print(df_filtrado)
 
-# Opcional: salvar como JSON
+# Salva em JSON com datas formatadas
 df_filtrado.to_json("dados_filtrados_2025.json", orient="records", indent=4, force_ascii=False)
+    
