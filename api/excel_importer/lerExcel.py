f4bcf1d45a8969fc5f8f7e62db5c809108b0c17d
RafaelDev <rafgam19@gmail.com>
2025-05-13 13:22:59 -0300

    Subido tudo.

--- api/excel_importer/lerExcel.py
+++ api/excel_importer/lerExcel.py
@@ -2,7 +2,7 @@ import pandas as pd
 from tabulate import tabulate
 
 # Caminho para o arquivo .xlsm
-caminho_arquivo = '/home/rafael/Documentos/Projetos/Projetos Feelancer/Projeto-Telemetria/api/excel_importer/dados2.xlsm'
+caminho_arquivo = '/home/rafael/Documentos/Projetos/Projetos Feelancer/Projeto-Telemetria/api/excel_importer/dados.xlsm'
 
 # Carrega todos os dados da primeira aba
 df = pd.read_excel(caminho_arquivo, engine='openpyxl')  # ou use engine='xlrd' se necessário
