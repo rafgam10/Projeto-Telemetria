f14e31a30f9f23437d2332c4f4f044d5c7016b25
RafaelDev <rafgam19@gmail.com>
2025-05-22 23:16:31 -0300

    Código do DB.

--- api/database/database_handler.py
+++ api/database/database_handler.py
@@ -10,79 +10,107 @@ def conectar():
 def criar_tabela():
     conec = conectar()
     cursor = conec.cursor()
-
-    sql = f"""
-        CREATE TABLE IF NOT EXISTS {NOMETABELA}(
-            
+    cursor.execute(f"DROP TABLE IF EXISTS {NOMETABELA}")
+    cursor.execute(f"""
+        CREATE TABLE IF NOT EXISTS {NOMETABELA} (
             motorista TEXT,
-
-            placa TEXT,
-            frota TEXT,
-            marca TEXT,
-
+            nr_acerto INTEGER,
             data TEXT,
-            datasSaida TEXT,
-            dataChegada TEXT,
-            qtdDias INTEGER,
-            totalHrs INTEGER,
-
-            KmSaida INTEGER,
-            KmChegada INTEGER,
-            KmRodado INTEGER,
-
-            LtArla REAL,
-            LtDiesel REAL,
-            LtPorDia REAL
-
-
+            data_saida TEXT,
+            data_chegada TEXT,
+            km_saida REAL,
+            km_chegada REAL,
+            km_rodado REAL,
+            km_vazio REAL,
+            porcento_vazio REAL,
+            qtd_dias INTEGER,
+            total_hrs INTEGER,
+            frota TEXT,
+            placa TEXT,
+            marca_modelo TEXT,
+            ano_veiculo INTEGER,
+            lt_diesel REAL,
+            media REAL,
+            lt_arla REAL,
+            porcento_arla REAL,
+            nr_equipamento INTEGER,
+            marca_modelo_equipamento TEXT,
+            ano_equipamento INTEGER,
+            lt_diesel_equip REAL,
+            media_1 REAL,
+            media_2 REAL,
+            lt_por_dia REAL,
+            km_rodado_dup REAL,
+            dif REAL,
+            media_dup REAL,
+            dif_media REAL
         )
-    """
+    """)
 
-    cursor.execute(sql)
+    # cursor.execute(sql)
 
     conec.commit()
     cursor.close()
 
 def inserir_dados(dados):
-    conec = conectar()
-    cursor = conec.cursor()
-
+    conec = conectar(); cursor = conec.cursor()
+    
+    # Insere os dados
     for item in dados:
         cursor.execute(f"""
             INSERT INTO {NOMETABELA} (
-                motorista, placa, frota, marca, data, datasSaida, dataChegada,
-                qtdDias, totalHrs, KmSaida, KmChegada, KmRodado,
-                LtArla, LtDiesel, LtPorDia
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                motorista, nr_acerto, data, data_saida, data_chegada,
+                km_saida, km_chegada, km_rodado, km_vazio, porcento_vazio,
+                qtd_dias, total_hrs, frota, placa, marca_modelo, ano_veiculo,
+                lt_diesel, media, lt_arla, porcento_arla,
+                nr_equipamento, marca_modelo_equipamento, ano_equipamento,
+                lt_diesel_equip, media_1, media_2,
+                lt_por_dia, km_rodado_dup, dif, media_dup, dif_media
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """, (
             item.get("MOTORISTA"),
-            item.get("Placa"),
-            item.get("Frota"),
-            item.get("Marca / Modelo"),
+            item.get("NR Acerto"),
             item.get("Data"),
             item.get("Data de Saida"),
             item.get("Data de Cheg."),
-            item.get("Qtd Dias"),
-            item.get("Tot. Hrs"),
             item.get("Km Saída"),
             item.get("Km Cheg."),
             item.get("Km Rod."),
-            item.get("Lt. Arla"),
+            item.get("Km Vazio"),
+            item.get("% Vazio"),
+            item.get("Qtd Dias"),
+            item.get("Tot. Hrs"),
+            item.get("Frota"),
+            item.get("Placa"),
+            item.get("Marca / Modelo"),
+            item.get("Ano"),
             item.get("Lt. Diesel"),
-            item.get("LT / DIA")
+            item.get("Méd."),
+            item.get("Lt. Arla"),
+            item.get("% ARLA"),
+            item.get("Nr. quipam."),
+            item.get("Marca / Modelo.1"),
+            item.get("Ano.1"),
+            item.get("Lt. Diesel.1"),
+            item.get("Méd..1"),
+            item.get("Méd..2"),
+            item.get("LT / DIA"),
+            item.get("KM RODADO"),
+            item.get("DIF"),
+            item.get("MEDIA"),
+            item.get("DIF. MEDIA")
         ))
+
 
     conec.commit()
     cursor.close()
 
 def obter_dados():
-    conec = conectar()
-    cursor = conec.cursor()
-
-    cursor.execute(f"SELECT * FROM {NOMETABELA}")
+    con = conectar()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM DadosTelemetria")
     dados = cursor.fetchall()
-    conec.close()
-
+    con.close()
     return dados
 
 def main():
