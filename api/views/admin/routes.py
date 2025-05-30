5448cefd0e3b6cbab229835ca1c9f3650ba7c7c8
Rafael <rafgam19@gmail.com>
2025-05-30 01:29:14 -0300

    Funcionalidade de editar os dados de motoristaAdmin e veiculosAdmin, feito com sucesso.

--- api/views/admin/routes.py
+++ api/views/admin/routes.py
@@ -1,6 +1,7 @@
 # admin/routes.py
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-from database.database_handler import motorista_dados, motorista_dados_unicos, veiculo_dados, veiculo_dados_unicos, dados_relatorios
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session, jsonify
+from database.database_handler import conectar, motorista_dados, motorista_dados_unicos, veiculo_dados, veiculo_dados_unicos, dados_relatorios
+import sqlite3
 import os
 
 admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
@@ -14,10 +15,61 @@ def pagina_gestao_motoristas():
     motoristas = motorista_dados_unicos()
     return render_template("motoristasAdmin.html", motoristas=motoristas)
 
+@admin_bp.route("/editar_motorista", methods=["POST"])
+def editar_motorista():
+    data = request.get_json()
+    id_original = data.get('placa')
+    motoristaDado =f"{data['id']} - {data['nome']}"
+
+    # Exemplo com SQLite
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE DadosTelemetria
+        SET motorista=?, marca_modelo=?, placa=?
+        WHERE placa=?
+    """, (
+        motoristaDado, data['caminhao'], data['placa'],
+        id_original
+    ))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"success": True})
+
+
 @admin_bp.route("/gestaoVeiculos", methods=["GET", "POST"])
 def pagina_gestao_veiculos():
     veiculos = veiculo_dados_unicos()
     return render_template("veiculosAdmin.html", veiculos=veiculos)
+
+@admin_bp.route("/editar_veiculos", methods=["POST"])
+def editar_veiculos():
+    data = request.get_json()
+    frota = data['frota']
+    modelo = data['modelo']
+    marca = data['marca']
+    placa = data['placa']
+    status = data['status']
+    placaId = data['placa']
+    marca_modelo = f"{marca} / {modelo}"
+
+    try:
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("""
+            UPDATE DadosTelemetria SET marca_modelo=?, placa=?, frota=?
+            WHERE placa=?
+        """, (marca_modelo, placa, frota, placaId))
+        conn.commit()
+        conn.close()
+        return jsonify({'mensagem': 'Veículo atualizado com sucesso'}), 200
+    except Exception as e:
+        print("Erro ao atualizar veículo:", e)
+        return jsonify({'erro': 'Erro interno'}), 500
+
 
 @admin_bp.route("/inserirDados", methods=["GET", "POST"])
 def pagina_inserir_dados():
