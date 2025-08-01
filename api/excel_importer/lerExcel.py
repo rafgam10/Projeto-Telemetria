84a9d55cedc5f114867fefc584e6ac03de6d883d
Rafael <rafgam19@gmail.com>
2025-08-01 16:04:08 -0300

    Código de Leitura dos Excels.

--- api/excel_importer/lerExcel.py
+++ api/excel_importer/lerExcel.py
@@ -2,7 +2,8 @@ import pandas as pd
 from tabulate import tabulate
 
 # Caminho para o arquivo
-caminho_arquivo = "/home/rafael/Documentos/Projetos/Projetos Feelancer/Projeto-Telemetria/uploads/base 3.xlsx"
+#caminho_arquivo = '/home/rafael/Documentos/Projetos/Projetos Feelancer/Projeto-Telemetria/uploads/2_SEMANA_complementadoTeste.xlsx'
+caminho_arquivo = '/home/rafael/Documentos/Projetos/Projetos Feelancer/Projeto-Telemetria/uploads/Teste-2 SEMANA .xlsx'
 
 # Lê o Excel, pulando as 2 primeiras linhas
 df = pd.read_excel(caminho_arquivo, engine="openpyxl", skiprows=2)
@@ -11,8 +12,11 @@ df = pd.read_excel(caminho_arquivo, engine="openpyxl", skiprows=2)
 df = df.dropna(how='all')
 
 # Exibe as colunas reais
-print("Colunas reais detectadas:")
-print(df.columns)
+print("Colunas reais detectadas:\n")
+print("Colunas lidas do Excel:", df.columns.tolist())
+print("Shape do DataFrame:", df.shape)
+
+
 
 # Exibe as 5 primeiras linhas formatadas
 print(tabulate(df.head(), headers='keys', tablefmt='psql'))
